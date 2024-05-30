328c9463f543f0a66fe8027672db0fb25a529092
paguist <paguistt@gmail.com>
2024-05-29 22:21:18 -0400

    pizzeria dojo js

--- pizzeriaDojo.js
+++ pizzeriaDojo.js
@@ -7,7 +7,7 @@ function pizzaOven(tipoCorteza, tipoSalsa, quesos, salsas) {
     return { tipoCorteza, tipoSalsa, quesos, salsas };
 }
 
-var pizza1 = pizzaOven(tiposCorteza[0],tiposSalsa[0], tiposQuesos[0], tiposSalsas[3, 4]);
+var pizza1 = pizzaOven(tiposCorteza[0],tiposSalsa[0], tiposQuesos[0], tiposSalsas[[3, 4]);
 console.log("Pizza 1:", pizza1);
 
 var pizza2 = pizzaOven(tiposCorteza[1], tiposSalsa[1], tiposQuesos[0,1], tiposSalsas[0, 1, 2]);
